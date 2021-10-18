ffc10e66f3fa298faa94e87661b24ec03ffe69f7
Lennart Espe <lnsp@users.noreply.github.com>
2021-10-18 22:34:43 +0200

    Don't return error from main

--- grep/src/main.rs
+++ grep/src/main.rs
@@ -3,7 +3,7 @@ use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, ErrorKind, Read, Write};
 
-fn main() -> Result<(), io::Error> {
+fn main() {
     // Get command line arguments
     let args: Vec<String> = env::args().collect();
 
@@ -12,16 +12,16 @@ fn main() -> Result<(), io::Error> {
     let matcher = Regex::new(query).expect("invalid pattern");
     let mut stdin = BufReader::new(match args.len() {
         2 => Box::new(io::stdin()) as Box<dyn Read>,
-        3 => Box::new(File::open(&args[2])?) as Box<dyn Read>,
+        3 => Box::new(File::open(&args[2]).expect("can't open file")) as Box<dyn Read>,
         _ => panic!("invalid number of args"),
     });
 
     // Start matching files
     let mut buffer = String::new();
     loop {
-        let bytes_read = stdin.read_line(&mut buffer)?;
+        let bytes_read = stdin.read_line(&mut buffer).unwrap();
         if bytes_read == 0 {
-            return Ok(());
+            return;
         }
         if matcher.is_match(buffer.trim()) {
             io::stdout()
